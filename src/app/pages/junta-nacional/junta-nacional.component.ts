026ca5cf5782b03ef81756ab80187bb32aa34aad
Malcolm Cordova <mercadocreativo@gmail.com>
2022-07-28 11:34:16 -0400

    actualizado directiva nacional

--- src/app/pages/junta-nacional/junta-nacional.component.ts
+++ src/app/pages/junta-nacional/junta-nacional.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Ceo } from '../../models/ceo';
+import { CeoService } from '../../services/ceo.service';
+
+import { Fotoceo } from '../../models/fotoceo';
+import { FotoceoService } from '../../services/fotoseo.service';
+
+import { HttpClient, HttpBackend } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-junta-nacional',
@@ -7,10 +16,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JuntaNacionalComponent implements OnInit {
 
-  constructor() { }
+
+  ceos: Ceo;
+  fotoceos: Fotoceo;
+  error: {};
+
+  private http: HttpClient;
+
+  constructor(
+    private ceoService: CeoService,
+    private fotoceoService: FotoceoService,
+
+    ) {
+   }
 
   ngOnInit() {
     window.scrollTo(0,0);
+    this.loadCeo();
+    this.loadFotoCeo();
+  }
+
+  loadCeo(){
+    this.ceoService.getCeos().subscribe(
+      (data: Ceo) => this.ceos = data,
+      error => this.error = error
+      );
+  }
+  loadFotoCeo(){
+    this.fotoceoService.getFotoceos().subscribe(
+      (data: Fotoceo) => this.fotoceos = data,
+      error => this.error = error
+      );
   }
 
 }
